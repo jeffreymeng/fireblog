8dbfe5bd497c65fc1f0896ce95bea223747a1726
jeffmeng <jkmexpress@yahoo.com>
2016-01-18 04:40:30 +0000

    t

--- js/fireblog.js
+++ js/fireblog.js
@@ -15,7 +15,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-function convertToHtml(text)
-   converter = new showdown.Converter();
+var converter = new showdown.Converter();
+function convertToHtml(text) {
    return converter.makeHtml(text)
 };
